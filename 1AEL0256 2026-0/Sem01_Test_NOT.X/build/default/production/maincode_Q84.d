032f990187c33ac25bc75aaba9564d1839ffa0aa
Kalun Lau <klnlau@gmail.com>
2026-01-22 17:39:17 -0500

    Add files via upload

---
+++ 1AEL0256 2026-0/Sem01_Test_NOT.X/build/default/production/maincode_Q84.d
@@ -0,0 +1,1 @@
+build/default/production\maincode_Q84.i: maincode_Q84.s cabecera.inc
---
+++ 1AEL0256 2026-0/Sem01_Test_NOT.X/maincode_Q84.s
@@ -0,0 +1,44 @@
+    PROCESSOR 18F57Q84
+    #include "cabecera.inc"
+    
+    PSECT upcino, class=CODE, reloc=2, abs
+upcino:
+    ORG 000000H	    ;Lo primero que el CPU ejecuta cuando se energiza, el vector de reset
+    bra configuro   ;salta a etiqueta configuro
+    
+    ORG 000100H	    ;Zona de programa de usuario
+configuro:
+    ;Conf del módulo de oscilador
+    movlb 0H	    ;Al Bank0
+    movlw 60H
+    movwf OSCCON1, 1	;NOSC=HFINTOSC, NDIV 1:1
+    movlw 02H
+    movwf OSCFRQ, 1	;HFINTOSC a 4MHz
+    movlw 40H
+    movwf OSCEN, 1	;HFINTOSC enabled
+    ;Conf las E/S
+    movlb 4H		;Al Bank4
+    bsf TRISB, 0, 1	;RB0 entrada
+    bcf ANSELB, 0, 1	;RB0 digital
+    bcf TRISD, 0, 1	;RD0 salida
+    bcf ANSELD, 0, 1	;RD0 digital
+    
+inicio:
+    btfss PORTB, 0, 1	;Pregunto si RB0 es uno
+    bra noescierto	;Falso a si RB0=1
+    bra escierto	;Verdad a si RB0=1
+    
+noescierto:
+    bsf LATD, 0, 1	;RD0 = 1
+    bra inicio		;salto a inicio
+    
+escierto:
+    bcf LATD, 0, 1	;RD0 = 0
+    bra inicio		;salto a inicio
+    
+    end upcino
+
+
+
+
+
